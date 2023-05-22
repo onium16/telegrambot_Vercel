a12ed81b936c3181f472f1835741e76c889b314a
onium16 <onium16@gmail.com>
2023-05-23 01:44:59 +0200

    add routers + requrements update

--- api/index.py
+++ api/index.py
@@ -1,11 +1,47 @@
+import os
 from flask import Flask
+import requests
+
+
+TOKEN = os.environ.get('TOKEN')
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+def parse_message(message):
+    print("message-->",message)
+    chat_id = message['message']['chat']['id']
+    txt = message['message']['text']
+    print("chat_id-->", chat_id)
+    print("txt-->", txt)
+    return chat_id,txt
+ 
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+                'chat_id': chat_id,
+                'text': text
+                }
+    r = requests.post(url,json=payload)
+    return r
+ 
+@app.route('/webhook', methods=['GET', 'POST'])
+def check_req():
+    if request.method == 'POST':
+        msg = request.get_json()
+        
+        chat_id,txt = parse_message(msg)
+        if txt == "hi":
+            tel_send_message(chat_id,"Hello!!")
+
+        else:
+            tel_send_message(chat_id,'from webhook')
+       
+        return Response('ok', status=200)
+    else:
+        return "<h1>Get 'GET' received!</h1>"
+ 
+
+@app.route("/", methods=['GET'])
+def index():
+    return "<h1>Welcome to telegrambot!</h1>"
