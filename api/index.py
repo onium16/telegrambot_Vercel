2a17f2ebc27d4fcd26ab9a2f6225f468e4f096dc
Volodymyrffs <57915696+Volodymyrffs@users.noreply.github.com>
2025-02-04 12:28:09 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,6 @@ from flask import Flask, Response, request, jsonify
 
 # Получаем токен из переменных окружения
 TOKEN = os.getenv('TOKEN')
-vercel_url = os.environ.get('VERCEL_URL')
 if not TOKEN:
     raise ValueError("Bot token is not set in environment variables!")
 
@@ -29,6 +28,7 @@ def parse_message(message):
 @app.route('/setwebhook', methods=['POST', 'GET'])
 def setwebhook():
     if request.method == 'POST':
+        vercel_url = os.environ.get('VERCEL_URL')
         webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={vercel_url}/webhook"
         response = requests.get(webhook_url)
         
