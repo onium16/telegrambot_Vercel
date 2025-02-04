82f1256cd17e53cc82dd7d9ac1795c47b5602f67
Volodymyrffs <57915696+Volodymyrffs@users.noreply.github.com>
2025-02-04 12:32:49 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -28,8 +28,7 @@ def parse_message(message):
 @app.route('/setwebhook', methods=['POST', 'GET'])
 def setwebhook():
     if request.method == 'POST':
-        vercel_url = os.environ.get('VERCEL_URL')
-        webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={vercel_url}/webhook"
+        webhook_url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={os.environ.get('VERCEL_URL')}/webhook"
         response = requests.get(webhook_url)
         
         if response.status_code == 200:
